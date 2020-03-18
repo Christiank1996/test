6f575d8ed5c925914d43d360459025d8aa0a1300
Christiank1996 <Christiank1996@hotmail.com>
2020-03-18 17:52:49 +0100

    t

--- testGit/src/Main.java
+++ testGit/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-        GUIWindow window = new GUIWindow("TEST JUNGE TEST");
+        GUIWindow window = new GUIWindow("TEST JUNGE TEEEST");
     }
 
 }
