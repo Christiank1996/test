6c08d1b1918d8d55407bdda3d78a4fc825159d95
Christiank1996 <Christiank1996@hotmail.com>
2020-03-18 13:13:11 +0100

    Hab das zum testen gemacht.

--- testGit/src/Main.java
+++ testGit/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-        GUIWindow window = new GUIWindow("TEST JUNGE TEEEEST");
+        GUIWindow window = new GUIWindow("TEST JUNGE TEST");
     }
 
 }
